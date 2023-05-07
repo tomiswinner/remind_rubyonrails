cf85a2837baacd741b9c80d0c9fc8a9513d373c3
omiswinner <mouseofsack911@gmail.com>
2023-05-07 17:03:05 +0900

    login&session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # deviseコントローラーを使用する際に、パラメータチェックする
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # Path: application_controller.rb
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :address])
+  end
 end
